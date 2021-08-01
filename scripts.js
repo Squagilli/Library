07fbc096eb152d875103db4a392d7af73370fdf0
Squagilli <cotrupeconnor@gmail.com>
2021-08-01 16:17:03 +0200

    adding update function to prevent duplication every time a new book is added

--- scripts.js
+++ scripts.js
@@ -7,9 +7,6 @@ const newBookForm = document.querySelector('.form-container');
 const span = document.getElementsByClassName('close')[0];
 const createBookButton = document.querySelector('.create-book');
 
-
-
-
 // The library itself
 let theLibrary = [];
 
@@ -23,73 +20,67 @@ function Book(title, author, pages, read) {
 
 // Adds book to library
 Book.prototype.addToLib = function() {
-    theLibrary.push(this)
+    theLibrary.push(this);
+
 }
-
-
-
-
 
 // Displays the library on cards
 function displayLib() {
-    theLibrary.forEach(function(theLibrary) {
-        console.table(theLibrary)
-        item = document.createElement('div');
-        bookTitle = document.createElement('h4');
-        bookAuthor = document.createElement('h4');
-        bookPages = document.createElement('h4');
-        bookRead = document.createElement('h4');
+            theLibrary.forEach(function(theLibrary) {
+            
+            console.table(theLibrary)
+            item = document.createElement('div');
+            bookTitle = document.createElement('h4');
+            bookAuthor = document.createElement('h4');
+            bookPages = document.createElement('h4');
+            bookRead = document.createElement('h4');
+            
+            item.classList.add('item');
+            bookTitle.textContent = `'${theLibrary.title}'`
+            bookAuthor.textContent = theLibrary.author
+            bookPages.textContent = `${theLibrary.pages} pages`
+    
+                if (theLibrary.read === true) {
+                    bookRead.textContent = 'Has been read';
+                }
+                else {
+                    bookRead.textContent = 'Has not been read';
+                }
+            
+                item.appendChild(bookTitle);
+                item.appendChild(bookAuthor);
+                item.appendChild(bookPages);
+                item.appendChild(bookRead);
+            
+                libContainer.append(item);
+    });
+};
 
-
-        item.classList.add('item');
-        bookTitle.textContent = `'${theLibrary.title}'`
-        bookAuthor.textContent = theLibrary.author
-        bookPages.textContent = `${theLibrary.pages} pages`
-        bookRead.textContent = theLibrary.read
-
-        item.appendChild(bookTitle);
-        item.appendChild(bookAuthor);
-        item.appendChild(bookPages);
-        item.appendChild(bookRead);
+// Updates library with new book
 
     
-        libContainer.append(item);
-    });
-}
+    
+
 
 // Example books
 const cruggus = new Book(
     'Cruggus',
     'The Crug',
     '987',
-    'have read',
+    true,
 )
 const ether = new Book(
     'The Ether',
     'Kromgar',
     '1152',
-    'have not read',
+    true,
 )
 const cooking = new Book(
     'Cookbook for Orcs',
     'Ogmar',
     '55',
-    'have read',
+    false,
 )
-
-
-
-// testing 
-/*console.log(cruggus);
-cruggus.addToLib();
-ether.addToLib();
-cooking.addToLib();
-console.table(theLibrary);
-
-console.log(cruggus.inLibrary)*/
-
-
-
 
 // Event listeners 
 newBookButton.onclick = function() {
@@ -110,7 +101,7 @@ const createBook = () => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const read = document.getElementById('read').value;
+    const read = document.getElementById('read').checked;
     return new Book(title, author, pages, read);
     
 }
@@ -119,7 +110,8 @@ const addBook = (e) => {
     e.preventDefault();
     const newBook = createBook();
     newBook.addToLib();
-    displayLib();
+    updateLibrary();
+
 
     console.log(newBook)
     newBookForm.style.display = 'none';
@@ -127,9 +119,39 @@ const addBook = (e) => {
     
 }
 
-createBookButton.addEventListener('click', addBook)
+function updateLibrary() {
+    console.table()
+            item = document.createElement('div');
+            bookTitle = document.createElement('h4');
+            bookAuthor = document.createElement('h4');
+            bookPages = document.createElement('h4');
+            bookRead = document.createElement('h4');
+            
+            item.classList.add('item');
+            bookTitle.textContent = `'${theLibrary.title}'`
+            bookAuthor.textContent = theLibrary.author
+            bookPages.textContent = `${theLibrary.pages} pages`
+    
+                if (theLibrary.read === true) {
+                    bookRead.textContent = 'Has been read';
+                }
+                else {
+                    bookRead.textContent = 'Has not been read';
+                }
+            
+                item.appendChild(bookTitle);
+                item.appendChild(bookAuthor);
+                item.appendChild(bookPages);
+                item.appendChild(bookRead);
+            
+                libContainer.append(item);
+}
 
+createBookButton.addEventListener('click', addBook);
 
 
 // displayLib();
+cruggus.addToLib();
+ether.addToLib();
+cooking.addToLib();
 displayLib();
